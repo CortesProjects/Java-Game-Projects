5b853d78a5af63989cdd171af8db1dce009127d9
Marc Benn B. Secong <marcbenn.secong@cit.edu>
2024-10-02 20:15:50 +0800

    part 3

--- Main.java
+++ Main.java
@@ -20,6 +20,8 @@ public class Main {
 
         System.out.println("another testing part 2!");
 
+        System.out.println("part 3!");
+
         switch (hero) {
             case 1:
                 obj.setCharacter(character[0], health[1], mana[0], money[0], exp[0]);
